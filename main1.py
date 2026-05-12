5dbe91d0959223048bb2916c87d0eb5ae4523f99
Zoe Qiwen <qiwenkolimntcode@gmail.com>
2026-05-12 14:46:36 +0700

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,10 @@
+from turtle import Turtle , Screen
+
+mini = Turtle()
+mini.color("green")
+mini.forward(100)
+screen_saya = Screen()
+print(screen_saya.canvheight)
+mini.shape('turtle')
+
+screen_saya.exitonclick()
